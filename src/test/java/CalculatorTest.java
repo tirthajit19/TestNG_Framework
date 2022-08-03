ae5add64d0619a8d6d8e26dd79c59223cbc7cb3f
Tirthajit Das <tirthajitd@testvagrant.com>
2022-08-03 15:45:26 +0530

    Annotations added

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,74 +1,82 @@
 
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class CalculatorTest {
-    @Test
+    Calculator calculator;
+    @BeforeMethod
+    private void setUp(){
+        calculator = new Calculator();
+    }
+
+    @Test(groups = {"simpleCalculation"})
     private void testAdditionOfTwoNumbers(){
         //Arrange
         int firstNumber = 5;
         int secondNumber = 10;
 
         //Act
-        Calculator calculator = new Calculator();
         int sum = calculator.add(firstNumber, secondNumber);
 
         //Assert
         Assert.assertEquals(sum, 15);
     }
 
-    @Test
+    @Test(groups = {"simpleCalculation"})
     private void testSubtractionOfTwoNumbers(){
         //Arrange
         int firstNumber = 10;
         int secondNumber = 5;
 
         //Act
-        Calculator calculator = new Calculator();
         int subtraction = calculator.subtract(firstNumber, secondNumber);
 
         //Assert
         Assert.assertEquals(subtraction, 5);
     }
 
-    @Test
+    @Test(groups = {"simpleCalculation"})
     private void testMultiplicationOfTwoNumbers(){
         //Arrange
         int firstNumber = 5;
         int secondNumber = 10;
 
         //Act
-        Calculator calculator = new Calculator();
         int multiplication = calculator.multiply(firstNumber, secondNumber);
 
         //Assert
         Assert.assertEquals(multiplication, 50);
     }
 
-    @Test
+    @Test(groups = {"complexCalculation"})
     private void testPercentageOfTwoNumbers(){
         //Arrange
         int firstNumber = 5;
         int secondNumber = 10;
 
         //Act
-        Calculator calculator = new Calculator();
         double percentage = calculator.percentage(firstNumber, secondNumber);
 
         //Assert
         Assert.assertEquals(percentage, 50.0);
     }
 
-    @Test
+    @Test(groups = {"complexCalculation"})
     private void testSquareOfANumber(){
         //Arrange
         int number = 5;
 
         //Act
-        Calculator calculator = new Calculator();
         int square = calculator.square(number);
 
         //Assert
         Assert.assertEquals(square, 25);
+    }
+
+    @AfterMethod
+    private void tearDown(){
+        System.out.println("Running after every test");
     }
 }
